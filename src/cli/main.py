1a191f1cf2c222f9e6e82e5a972f712e71db3750
kimsengduong <duong.kim.seng@gmail.com>
2023-12-14 00:24:13 +0700

    alias alembic cli

--- src/cli/main.py
+++ src/cli/main.py
@@ -1,5 +1,38 @@
+import click
+from alembic.config import main as alembic_main
 from uvicorn import run
 
 
+@click.group()
 def main():
+    return
+
+
+@main.command()
+def start():
+    """Start the server."""
     run("src.app:app", host="0.0.0.0", port=8000, reload=True)
+
+
+@main.command(name="upgrade")
+def upgrade():
+    """Migrate the database."""
+    alembic_main(argv=["upgrade", "head"])
+
+
+@main.command()
+@click.argument("revision", required=True)
+def downgrade(revision):
+    """Downgrade the database."""
+    alembic_main(argv=["downgrade", revision])
+
+
+@main.command()
+@click.option("--message", required=True)
+def revision(message):
+    """Create a new revision."""
+    alembic_main(argv=["revision", "--autogenerate", "-m", message])
+
+
+if __name__ == "__main__":
+    main()
--- src/migrations/env.py
+++ src/migrations/env.py
@@ -4,14 +4,12 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from alembic import context
 
-import colorama
 
 # ---------------- added code here -------------------------#
 import os, sys
 from dotenv import load_dotenv
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print(colorama.Fore.GREEN, "BASE_DIR: ", BASE_DIR, colorama.Fore.RESET)
 load_dotenv(os.path.join(BASE_DIR, ".env"))
 sys.path.append(BASE_DIR)
 # ------------------------------------------------------------#
