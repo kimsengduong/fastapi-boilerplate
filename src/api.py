4cd3a94767c88a228641655e3492f9e97de393df
kimsengduong <duong.kim.seng@gmail.com>
2023-12-20 12:35:10 +0700

    updated root route

--- src/api.py
+++ src/api.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter
 
 from src.user.api import router as user_router
+from .config import APP_NAME, APP_VERSION, APP_CONTACT
 
 api_router = APIRouter()
 
 
 @api_router.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"name": APP_NAME, "version": APP_VERSION, "contact": APP_CONTACT}
 
 
 api_router.include_router(
